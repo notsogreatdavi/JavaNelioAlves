c99d7a1565709e8f84d879b23f93049edc56259c
Davi <davivdsilva03@gmail.com>
2025-02-24 19:20:33 -0300

    Exercicio04
    Exercício 04 do curso feito

---
+++ src/Exercicio04.java
@@ -0,0 +1,22 @@
+package src;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int funcionario, horast;
+        double valorhora, salario;
+
+        funcionario = sc.nextInt();
+        horast = sc.nextInt();
+        valorhora = sc.nextDouble();
+
+        salario = valorhora * horast;
+
+        System.out.printf("NUMBER = %d\nSALARY = U$ %.2f", funcionario, salario);
+
+        sc.close();
+    }
+}
