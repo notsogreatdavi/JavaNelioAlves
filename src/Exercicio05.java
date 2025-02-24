49b9598f2f58433ab2d0c02b0316768a03eb7948
Davi <davivdsilva03@gmail.com>
2025-02-24 19:30:29 -0300

    Exercicio05
    Exercício 05 do curso feito

---
+++ src/Exercicio05.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.util.Scanner;
+
+public class Exercicio05 {
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int keyCode1, keyQuant1, keyCode2, keyQuant2;
+        double keyValue1, keyValue2, total;
+
+        keyCode1 = sc.nextInt();
+        keyQuant1 = sc.nextInt();
+        keyValue1 = sc.nextDouble();
+        keyCode2 = sc.nextInt();
+        keyQuant2 = sc.nextInt();
+        keyValue2 = sc.nextDouble();
+
+        total = (keyValue1 * keyQuant1) + (keyValue2 * keyQuant2);
+
+        System.out.printf("VALOR A PAGAR: R$ %.2f", total);
+
+        sc.close();
+    }
+}
