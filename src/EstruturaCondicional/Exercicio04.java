fc3cdc1cb9724baade61629c092209434d0f4651
Davi <davivdsilva03@gmail.com>
2025-02-25 16:07:04 -0300

    Exercicio04 Condicional

---
+++ src/EstruturaCondicional/Exercicio04.java
@@ -0,0 +1,22 @@
+package src.EstruturaCondicional;
+
+import java.util.Scanner;
+
+public class Exercicio04 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        int start, end, duration;
+
+        start = sc.nextInt();
+        end = sc.nextInt();
+
+        if (start < end) {
+            duration = end - start;
+        }
+        else {
+            duration = 24 - start + end;
+        }
+        System.out.printf("O JOGO DUROU %d HORA(S)", duration);
+    }
+}
