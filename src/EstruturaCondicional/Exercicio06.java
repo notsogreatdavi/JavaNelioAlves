a5d1c196237c2ca6b007fdb0f4bdbbf9494a81ea
Davi <davivdsilva03@gmail.com>
2025-02-26 18:25:56 -0300

    Exercicio06 Condicional Fix
    Após iterar a primeira condição não é preciso definir o piso do intervalo para os próximos

--- src/EstruturaCondicional/Exercicio06.java
+++ src/EstruturaCondicional/Exercicio06.java
@@ -8,16 +8,19 @@ public class Exercicio06 {
 
         double num = sc.nextDouble();
 
-        if (num >= 0 && num <= 25) {
+        if (num < 0 || num > 100) {
             System.out.println("Intervalo [0,25]");
         }
-        else if (num > 25 && num <= 50) {
+        else if (num >= 0 && num <= 25) {
+            System.out.println("Intervalo [0,25]");
+        }
+        else if (num <= 50) {
             System.out.println("Intervalo (25, 50]");
         }
-        else if (num > 50 && num <= 75) {
+        else if (num <= 75) {
             System.out.println("Intervalo (50,75]");
         }
-        else if (num > 75 && num <= 100) {
+        else if (num <= 100) {
             System.out.println("Intervalo (75,100]");
         }
         else {
